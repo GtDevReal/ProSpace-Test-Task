45d2f368b8e70ac67a8b0c5f7ce60ef247f27ce6
GtDevReal <21Bugaga21@mail.ru>
2025-01-25 20:23:11 +0300

    Создание проекта

---
+++ ProSpaceTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProSpaceTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
